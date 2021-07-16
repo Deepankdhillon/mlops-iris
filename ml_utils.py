885bb991cf521d0e101324a5674d2c34697b3f27
deepank <deepank@acpl.co.in>
2021-07-17 00:08:58 +0530

    assingment complete

--- ml_utils.py
+++ ml_utils.py
@@ -2,9 +2,12 @@ from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score
+from sklearn.tree import DecisionTreeClassifier
+
 
 # define a Gaussain NB classifier
-clf = GaussianNB()
+#clf = GaussianNB()
+clf = DecisionTreeClassifier()
 
 # define the class encodings and reverse encodings
 classes = {0: "Iris Setosa", 1: "Iris Versicolour", 2: "Iris Virginica"}
--- test_app.py
+++ test_app.py
@@ -24,3 +24,32 @@ def test_pred_virginica():
         # asserting the correct response is received
         assert response.status_code == 200
         assert response.json() == {"flower_class": "Iris Virginica"}
+
+
+def test_pred_versicolour():
+    # defining a sample payload for the testcase
+    payload = {
+        "sepal_length": 2,
+        "sepal_width": 5,
+        "petal_length": 2,
+        "petal_width": 1,
+    }
+    with TestClient(app) as client:
+        response = client.post("/predict_flower", json=payload)
+        # asserting the correct response is received
+        assert response.status_code == 200
+        assert response.json() == {"flower_class": "Iris Versicolour"}
+
+def test_pred_setosa():
+    # defining a sample payload for the testcase
+    payload = {
+        "sepal_length": 1,
+        "sepal_width": 2,
+        "petal_length": 2,
+        "petal_width": 1,
+    }
+    with TestClient(app) as client:
+        response = client.post("/predict_flower", json=payload)
+        # asserting the correct response is received
+        assert response.status_code == 200
+        assert response.json() == {"flower_class": "Iris Setosa"}
